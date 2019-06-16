3db80aecd413425dc85d1f12073c3338e194cefb
david <david@david-rMBP.local>
2019-06-16 20:02:48 +0100

    nav promotion and favourites

---
+++ script.js
@@ -0,0 +1,17 @@
+const burger = document.querySelector("div[data-behavior='burger']")
+const nav = document.querySelector("ul[data-behavior='nav_links']")
+const navLinks = document.querySelectorAll("li[data-behavior='nav_list']")
+
+burger.addEventListener('click', () => {
+  nav.classList.toggle('c-nav--active')
+
+  navLinks.forEach((link, index) => {
+    if (link.style.animation) {
+      link.style.animation = ''
+    } else {
+      link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.2}s`
+    }
+  })
+  
+  burger.classList.toggle('toggle')
+})
