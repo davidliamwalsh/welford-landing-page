7d77624ff0ca281e8acebd893197fa5b8bac7993
david <david@david-rMBP.local>
2019-06-21 00:08:31 +0100

    nav ease-in transition

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ burger.addEventListener('click', () => {
     if (link.style.animation) {
       link.style.animation = ''
     } else {
-      link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.2}s`
+      link.style.animation = `navLinkFade 1s ease forwards ${index / 7 + 0.2}s`
     }
   })
   
